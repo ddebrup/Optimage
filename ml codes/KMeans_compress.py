0d9ed9d7dc1f6fb7d4f912242969cfd26cfa8830
Aditya_Gupta <b518006@iiit-bh.ac.in>
2020-11-17 18:38:04 +0530

    buffrt

--- ml codes/KMeans_compress.py
+++ ml codes/KMeans_compress.py
@@ -11,24 +11,23 @@ io.show()
 
 rows = image.shape[0]
 cols = image.shape[1]
- 
-image = image.reshape(image.shape[0]*image.shape[1],3)
-kmeans = KMeans(n_clusters = 10, n_init=10, max_iter=200)
+
+image = image.reshape(image.shape[0]*image.shape[1], 3)
+kmeans = KMeans(n_clusters=10, n_init=10, max_iter=200)
 kmeans.fit(image)
 
-centers = np.asarray(kmeans.cluster_centers_,dtype=np.uint8) 
-labels = np.asarray(kmeans.labels_,dtype=np.uint8 )  
-labels = labels.reshape(rows,cols); 
+centers = np.asarray(kmeans.cluster_centers_, dtype=np.uint8)
+labels = np.asarray(kmeans.labels_, dtype=np.uint8)
+labels = labels.reshape(rows, cols)
 
-np.savez_compressed('codebook_sample.npz',centers)    
-#io.imsave('compressed_dog.jpg',labels);
-np.savez_compressed('compressed_sample.npz',labels);
+np.savez_compressed('codebook_sample.npz', centers)
+# io.imsave('compressed_dog.jpg',labels);
+np.savez_compressed('compressed_sample.npz', labels)
 
 
 #image = np.zeros((labels.shape[0],labels.shape[1],3),dtype=np.uint8 )
-#for i in range(labels.shape[0]):
+# for i in range(labels.shape[0]):
 #    for j in range(labels.shape[1]):
 #            image[i,j,:] = centers[labels[i,j],:]
-#            
-#io.imsave('reconstructed_dog.jpg',image);           
-    
+#
+# io.imsave('reconstructed_dog.jpg',image);
