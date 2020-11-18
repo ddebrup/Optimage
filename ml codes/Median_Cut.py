bffcca1e813f19973a1c24fa46732243fe6a9c35
Subham Sharma <subhamsharma@outlook.com>
2020-11-18 10:32:16 +0530

    debugging changes

--- ml codes/Median_Cut.py
+++ ml codes/Median_Cut.py
@@ -1,32 +1,13 @@
-import argparse
+#import argparse
 import math
+import sys
 
 import numpy as np
 from skimage.io import imread, imsave
 from skimage.color import rgba2rgb
 
-parser = argparse.ArgumentParser(description='Perform Median Cut Color Quantization on image.')
-parser.add_argument('-c', '--colors', type=int,
-                    help='Number of colors needed in power of 2, ex: for 16 colors pass 4 because 2^4 = 16')
-parser.add_argument('-i', '--input', type=str, help='path of the image to be quantized')
-parser.add_argument('-o', '--output', type=str, help='output path for the quantized image')
 
-# get the arguments
-args = parser.parse_args()
 
-# get the values from the arguments
-colors = args.colors
-print("reducing the image to {} color palette".format(int(math.pow(2, colors))))
-
-output_path = args.output
-input_path = args.input
-
-# read the image
-sample_img = imread(input_path)
-if sample_img.shape[2]==4:
-	sample_img=rgba2rgb(sample_img)
-	imsave('con.png',sample_img)
-	sample_img = imread('con.png')
 
 
 def median_cut_quantize(img, img_arr):
@@ -36,7 +17,7 @@ def median_cut_quantize(img, img_arr):
     b_average = np.mean(img_arr[:, 2])
 
     for data in img_arr:
-        sample_img[data[3]][data[4]] = [r_average, g_average, b_average]
+        img[data[3]][data[4]] = [r_average, g_average, b_average]
 
 
 def split_into_buckets(img, img_arr, depth):
@@ -69,16 +50,45 @@ def split_into_buckets(img, img_arr, depth):
     split_into_buckets(img, img_arr[0:median_index], depth - 1)
     split_into_buckets(img, img_arr[median_index:], depth - 1)
 
+	
+	
+def main(argv):	
+	#parser = argparse.ArgumentParser(description='Perform Median Cut Color Quantization on image.')
+	#parser.add_argument('-c', '--colors', type=int,
+		           # help='Number of colors needed in power of 2, ex: for 16 colors pass 4 because 2^4 = 16')
+	#parser.add_argument('-i', '--input', type=str, help='path of the image to be quantized')
+	#parser.add_argument('-o', '--output', type=str, help='output path for the quantized image')
 
-flattened_img_array = []
-for rindex, rows in enumerate(sample_img):
-    for cindex, color in enumerate(rows):
-        flattened_img_array.append([color[0], color[1], color[2], rindex, cindex])
+	# get the arguments
+	#args = parser.parse_args()
 
-flattened_img_array = np.array(flattened_img_array)
+	# get the values from the arguments
+	colors = int(sys.argv[1])
+	print("reducing the image to {} color palette".format(int(math.pow(2, colors))))
 
-# start the splitting process
-split_into_buckets(sample_img, flattened_img_array, colors)
+	input_path = sys.argv[2]
+	output_path = sys.argv[3]
+	
+	
+		# read the image
+	sample_img = imread(input_path)
+	if sample_img.shape[2]==4:
+		sample_img=rgba2rgb(sample_img)
+		imsave('con.png',sample_img)
+		sample_img = imread('con.png')
 
-# save the final image
-imsave(output_path, sample_img)
+
+	flattened_img_array = []
+	for rindex, rows in enumerate(sample_img):
+		for cindex, color in enumerate(rows):
+			flattened_img_array.append([color[0], color[1], color[2], rindex, cindex])
+
+	flattened_img_array = np.array(flattened_img_array)
+
+	# start the splitting process
+	split_into_buckets(sample_img, flattened_img_array, colors)
+
+	# save the final image
+	imsave(output_path, sample_img)
+	
+main(sys.argv)
